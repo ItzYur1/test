e41aafa6f934e0d85e038e1fea2bd5d5f6cd18c0
ItzYur1 <abramuri9@gmail.com>
2024-12-24 22:54:10 +0800

    Update Testing.lua

--- Testing.lua
+++ Testing.lua
@@ -74,7 +74,7 @@ local BSection = MTab:CreateSection("Main ESP")
 
    local MToggle = MTab:CreateToggle({
       Name = "Player ESP",
-      CurrentValue = true,
+      CurrentValue = false,
       Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
       Callback = function(Value)
          _G.ESPToggle = Value 
